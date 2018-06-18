51851b048a1f361e816a3fcbbfc7d21262604299
aerosat99 <40327273+aerosat99@users.noreply.github.com>
2018-06-19 00:02:42 +0900

    Update 1.py

--- 1.py
+++ 1.py
@@ -5,30 +5,40 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 size = int(input('Max = '))
 
-list0 = list(range(1, size + 1))   # list(range(1000)) -> 0~999까지 1000개 / list(range(1,1001)) -> 1부터 1000까지 1000
+list0 = list(range(1,size+1))  # list(range(1000)) -> 0~999까지 1000개 / list(range(1,1001)) -> 1부터 1000까지 1000
 
 def make_3m_list(size) :
-    list3m = []
+    list1 = []
     for v in list0 :
         if v % 3 == 0 :
-            list3m.append(v)
-    return list3m
+            list1.append(v)
+    return set(list1)
 
 def make_5m_list(size) :
-    list5m = []
+    list2 = []
     for v in list0 :
         if v % 5 == 0 :
-            list5m.append(v)
-    return list5m
+            list2.append(v)
+    return set(list2)
 
-set3m = set(make_3m_list(size))
-set5m = set(make_5m_list(size))
-set3_5m = set3m | set5m           # 중복을 한개만 놔두고 합집합으로 
-print('3 : %d / 5 : %d / 3 or 5 : %d'%(len(set3m), len(set5m), len(set3_5m))) # 집합의 원소 개수는 len으로 센다
+def make_15m_list(size) :
+    list3 = []
+    for v in list0 :
+        if v % 15 == 0 :
+            list3.append(v)
+    return set(list3)
 
-list_t = list(set3_5m)
 
-summ = 0
-for v in list_t :
-    summ += v 
-print(summ)
+set3 = make_3m_list(size)
+set5 = make_5m_list(size)
+set15 = make_15m_list(size)
+set_t = set3 | set5 | set15    # 중복을 한개만 놔두고 합집합으로
+
+list_set_t = list(set_t)
+
+print('3 = %s / 5 = %s / 3 or 5 = %s / Total = %s'%(len(set3), len(set5), len(set15), len(set_t)))  # 집합의 원소 개수는 len으로 센다
+
+sum = 0
+for v in list_set_t :
+    sum += v
+print('Sum = %s'%sum)
